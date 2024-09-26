a27821b91d1a294185aa18d1ca1242db125332d4
Daniel Oaks <daniel@danieloaks.net>
2024-09-26 17:57:01 +1000

    Add noisy rusty footer bg

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -7,8 +7,8 @@ import { LinkList } from "../app/internalLinks";
 function TheFooter() {
   return (
     <>
-      <footer className="bg-rustyRed-700 text-slate-100 px-10 py-6 flex items-center justify-between">
-        <div className="text-sm flex gap-1 flex-col">
+      <footer className="bg-rustyRed-700 text-slate-100 px-10 py-6 flex items-center justify-between relative">
+        <div className="text-sm flex gap-1 flex-col z-10">
           {LinkList.map((l, i) => (
             <Link to={l.path} className="flex gap-1.5 items-center" key={i}>
               <FeatherIcon icon={l.icon} size="18" />
@@ -16,11 +16,11 @@ function TheFooter() {
             </Link>
           ))}
         </div>
-        <div className="flex text-sm gap-1 flex-col text-center">
+        <div className="flex text-sm gap-1 flex-col text-center z-10">
           <p>Presented by Dr Helen Berents</p>
           <p>Web design and hosting // Daniel Oakley</p>
         </div>
-        <div className="flex text-sm gap-1 flex-col">
+        <div className="flex text-sm gap-1 flex-col z-10">
           <img src={GriffithLogo} className="h-16"/>
         </div>
       </footer>
