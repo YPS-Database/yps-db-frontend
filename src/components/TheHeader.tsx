49bade40fe50f0a2df01285027254a24fbbdb951
Daniel Oaks <daniel@danieloaks.net>
2024-10-31 05:44:55 +1000

    Make header dark, to unify experience some

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import FeatherIcon from "feather-icons-react";
 
-import logo from "../assets/logo.png";
+import logoLight from "../assets/logoLight.png";
 import { LinkList } from "../app/internalLinks";
 import { useAppDispatch, useAppSelector } from "../app/store";
 import { nextColourMode } from "../app/preferencesSlice";
@@ -15,14 +15,14 @@ function TheHeader() {
 
   return (
     <header className="sticky top-0">
-      <div className="flex items-center justify-between bg-whiteIce-100 px-10 py-1 dark:bg-whiteIce-700">
+      <div className="flex items-center justify-between bg-whiteIce-700 px-10 py-1 dark:bg-whiteIce-700">
         <Link to="/" className="flex items-center gap-3 px-2 py-2">
-          <img src={logo} alt="YPS Database logo" className="h-14" />
+          <img src={logoLight} alt="YPS Database logo" className="h-14" />
         </Link>
-        <div className="flex gap-2 text-slate-900 dark:text-slate-100">
+        <div className="flex gap-2 text-slate-100">
           {displayColourModeSwitcher && (
             <button
-              className="rounded-lg border-2 border-solid border-whiteIce-200 bg-whiteIce-50 p-2.5 dark:border-whiteIce-800 dark:bg-whiteIce-600"
+              className="rounded-lg border-2 border-solid border-whiteIce-500 bg-whiteIce-600 bg-opacity-50 p-2.5 text-slate-100"
               onClick={() => {
                 dispatch(nextColourMode());
               }}
@@ -40,21 +40,21 @@ function TheHeader() {
           )}
           <Link
             to="/"
-            className="rounded-lg border-2 border-solid border-whiteIce-200 bg-whiteIce-50 p-2.5 text-slate-900 dark:border-whiteIce-800 dark:bg-whiteIce-600 dark:text-slate-100"
+            className="rounded-lg border-2 border-solid border-whiteIce-500 bg-whiteIce-600 bg-opacity-50 p-2.5 text-slate-100"
           >
             <FeatherIcon icon="search" />
           </Link>
           {(user.level === "admin" || user.level === "superuser") && (
             <Link
               to="admin"
-              className="rounded-lg border-2 border-solid border-whiteIce-200 bg-whiteIce-50 p-2.5 text-slate-900 dark:border-whiteIce-800 dark:bg-whiteIce-600 dark:text-slate-100"
+              className="rounded-lg border-2 border-solid border-whiteIce-500 bg-whiteIce-600 bg-opacity-50 p-2.5 text-slate-100"
             >
               <FeatherIcon icon="settings" />
             </Link>
           )}
         </div>
       </div>
-      <nav className="flex flex-wrap items-center justify-start gap-x-6 gap-y-2 bg-whiteIce-700 px-10 py-2 text-sm dark:bg-whiteIce-900">
+      <nav className="flex flex-wrap items-center justify-start gap-x-6 gap-y-2 bg-whiteIce-800 px-10 py-2 text-sm dark:bg-whiteIce-900">
         {LinkList.map((l, i) => (
           <Link to={l.path} className="flex items-center gap-1.5" key={i}>
             <FeatherIcon icon={l.icon} size="18" />
