df84b7cef939fcf702a061b10412d33530c7696d
Daniel Oaks <daniel@danieloaks.net>
2024-10-31 13:26:12 +1000

    Support more types of filters

--- src/app/utilities.ts
+++ src/app/utilities.ts
@@ -2,8 +2,12 @@ export function improveFilterName(input: string): string {
   switch (input) {
     case "entry_type":
       return "Entry type";
+    case "region":
+      return "Region";
     case "year":
       return "Year";
+    case "youth_led":
+      return "Youth-led / Authored";
     default:
       return input;
   }
